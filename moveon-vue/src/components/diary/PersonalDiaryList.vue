4530e71f1d0f767b6ca15cd4b0d257b177cb6a8c
Moveon <moveon_0827@outlook.com>
2022-01-30 23:27:25 +0800

    update project

--- moveon-vue/src/components/diary/PersonalDiaryList.vue
+++ moveon-vue/src/components/diary/PersonalDiaryList.vue
@@ -1,8 +1,8 @@
 <template>
-  <div :onload="init()">
+  <div>
     <el-row>
       <el-col>
-        <el-button type="primary" size="small" @click="writeDiary()">填写日记</el-button>
+        <el-button type="primary" size="small" @click="dialogFormVisible = true">填写日记</el-button>
       </el-col>
     </el-row>
     <el-table :data="DiaryList">
@@ -14,23 +14,45 @@
         <el-button type="text" size="small">编辑</el-button>
       </template>
     </el-table>
+
+    <el-dialog title="填写日记" :visible.sync = "dialogFormVisible">
+      <el-form :model="diaryForm">
+        <el-input type="text">请输入日记标题</el-input>
+        <el-input type="text">请填写日记时间</el-input>
+        <el-input type="text">请填写日记作者</el-input>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确认</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
   export default {
+    data() {
+      return {
+        dialogFormVisible: false,
+        DiaryList: [
+          {diaryTitle: 'a', diaryDate: 'a', diaryAuthor: 'a'}
+        ],
+        diaryForm: {}
+      }
+    },
     name: "PersonalDiaryList",
-    data: {
-      DiaryList: [],
+    mounted() {
+      this.loadTable()
     },
     methods: {
-      init() {
-        console.log("a")
+      loadTable() {
+        const that = this
         let gUserId = sessionStorage.getItem("globalUserId");
         this.$http.get('/diary/getUserDiary', {params: {userId: gUserId}})
           .then(res => {
-            this.DiaryList.push(res.data.data)
-            console.log(res.data)
+            that.DiaryList = []
+            that.DiaryList = res.data.data
+            console.log(that.DiaryList)
           })
       },
       writeDiary() {
